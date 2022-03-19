b99c85b62f91bedf222c722a8854b0a487fd9a15
Thế Dương <theduongctn@gmail.com>
2022-03-19 18:50:37 +0700

    Add toFixed(2) to tipEachPerson

--- 04.JavaScript/Buoi05/homework2/script.js
+++ 04.JavaScript/Buoi05/homework2/script.js
@@ -9,13 +9,9 @@ $(function () {
 
   function tipEachPerson() {
     let tipPercent = Number($inputQuality.val());
-    console.log("🚀 ~ file: script.js ~ line 12 ~ tipEachPerson ~ tipPercent", tipPercent)
     let tipAmount = Number($inputAmount.val()) * (tipPercent / 100);
-    console.log("🚀 ~ file: script.js ~ line 14 ~ tipEachPerson ~ tipAmount", tipAmount)
     let guests = Number($inputGuests.val());
-    console.log("🚀 ~ file: script.js ~ line 16 ~ tipEachPerson ~ guests", guests)
     let tipEach = tipAmount / guests;
-    console.log("🚀 ~ file: script.js ~ line 18 ~ tipEachPerson ~ tipEach", tipEach)
-    $tipEachPerson.text(tipEach);
+    $tipEachPerson.text(tipEach.toFixed(2));
   }
 });
