332b7d2fada082e81fb6a7b0e719c100444d692e
theduonghn <theduongctn@gmail.com>
2022-04-01 21:59:36 +0700

    Improve leetcode 1662

--- 05.CTDLGT/Buoi03/Bai1662.java
+++ 05.CTDLGT/Buoi03/Bai1662.java
@@ -1,16 +1,8 @@
 public class Bai1662 {
     public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String string1 = join(word1);
-        String string2 = join(word2);
+        String string1 = String.join("", word1);
+        String string2 = String.join("", word2);
         return string1.equals(string2);
-    }
-
-    public static String join(String[] arr) {
-        String result = "";
-        for (String s : arr) {
-            result = result.concat(s);
-        }
-        return result;
     }
 
     public static void main(String[] args) {
