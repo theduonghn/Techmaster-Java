cc689b9ad4f12551478376c6363226ea5706f4d8
theduonghn <theduongctn@gmail.com>
2022-05-13 23:50:26 +0700

    SpringBoot lesson 1: Exercise

--- 06.SpringBoot/Buoi01/demo/src/main/java/com/techmaster/demo/controller/HelloController.java
+++ 06.SpringBoot/Buoi01/demo/src/main/java/com/techmaster/demo/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.techmaster.demo.controller;
 
 import com.techmaster.demo.model.Book;
+import com.techmaster.demo.model.Message;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,5 +49,11 @@ public class HelloController {
     @GetMapping("/add")
     public int add2(@RequestParam("a") int a, @RequestParam("b") int b) {
         return a + b;
+    }
+
+    @PostMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Message echoMessage(@RequestBody Message message) {
+        return message;
     }
 }
---
+++ 06.SpringBoot/Buoi01/demo/src/main/java/com/techmaster/demo/model/Message.java
@@ -0,0 +1,5 @@
+package com.techmaster.demo.model;
+
+public record Message(String title, String content) {
+
+}
