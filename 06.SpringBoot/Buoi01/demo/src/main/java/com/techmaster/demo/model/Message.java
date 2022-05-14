8fa9e0cc358bf108df0d9f311fcd1c82991e0f92
theduonghn <theduongctn@gmail.com>
2022-05-14 11:55:55 +0700

    SpringBoot lesson 1: Practice

--- 06.SpringBoot/Buoi01/demo/src/main/java/com/techmaster/demo/model/Message.java
+++ 06.SpringBoot/Buoi01/demo/src/main/java/com/techmaster/demo/model/Message.java
@@ -1,5 +1,28 @@
 package com.techmaster.demo.model;
 
-public record Message(String title, String content) {
+public class Message {
+    private String title;
+    private String content;
+
+    public Message(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 
 }
